d4baf3915fce11ec486a49b80e36d1204952fe40
Karthikb777 <karthik.barati.777@gmail.com>
2021-05-01 11:05:34 +0530

    built create, edit note activity

---
+++ app/src/main/java/com/karthik/blissv2alpha10/CreateEditNoteReminderActivity.kt
@@ -0,0 +1,11 @@
+package com.karthik.blissv2alpha10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateEditNoteReminderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_edit_note_reminder)
+    }
+}
--- app/src/main/java/com/karthik/blissv2alpha10/MainActivity.kt
+++ app/src/main/java/com/karthik/blissv2alpha10/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.karthik.blissv2alpha10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -13,8 +14,10 @@ import com.karthik.blissv2alpha10.ui.NoteHomeLayout
 import com.karthik.blissv2alpha10.ui.ReminderHomeLayout
 import com.karthik.blissv2alpha10.ui.TodoHomeLayout
 import com.karthik.blissv2alpha10.ui.viewModels.HomeViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var currentFrag: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,8 +28,28 @@ class MainActivity : AppCompatActivity() {
 
         val homeViewModel : HomeViewModel by viewModels()
 
+        fabAdd.setOnClickListener {
+            when(currentFrag) {
+                0 -> {
+                    val newNoteIntent = Intent(this, CreateEditNoteReminderActivity::class.java)
+                    startActivity(newNoteIntent)
+                }
+                1 -> {
+                    val newReminderIntent = Intent(this, CreateEditNoteReminderActivity::class.java)
+                    startActivity(newReminderIntent)
+                }
+                2 -> {
+//                        TODO: change this to a new todo intent after creating new todo activity
+                    val newNoteIntent = Intent(this, CreateEditNoteReminderActivity::class.java)
+                    startActivity(newNoteIntent)
+                }
+            }
+        }
+
         homeViewModel.getCurrent().observe(this, Observer {
 //            TODO: use this to remove the search bar fragment while displaying the view fragments
+            currentFrag = it
+
         })
     }
 }
